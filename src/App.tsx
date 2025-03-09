9eb0cd3e50745ef828bed86fc075cab79ba85c41
eliott <eliott.jaquier@gmail.com>
2025-03-09 21:16:07 +0100

    feat: remove UserDetails route from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import GroupDetails from './pages/GroupDetails.jsx'
-import UserDetails from './pages/UserDetails.jsx'
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/group/:id" element={<GroupDetails />} />
-        <Route path="/user/:id" element={<UserDetails />} />
       </Routes>
     </Router>
   )
