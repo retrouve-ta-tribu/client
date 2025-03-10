1dab064e06950a656123d0ff9b6cddcade22c2aa
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-10 15:29:08 +0100

    feat: add template google login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import GroupDetails from './pages/GroupDetails.jsx'
-import Login from './pages/Login.jsx'
+import Login from './pages/Login.tsx'
 
 function App() {
   return (
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,25 +1,62 @@
-import { FC } from 'react'
-import PageContainer from '../components/layout/PageContainer'
-import PageHeader from '../components/layout/PageHeader'
-import { GoogleLogin } from '@react-oauth/google';
+import { FC, useState, useEffect } from 'react';
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import PageContainer from '../components/layout/PageContainer';
+import PageHeader from '../components/layout/PageHeader';
 
 const Login: FC = () => {
+    const [user, setUser] = useState(null);
+    const [profile, setProfile] = useState(null);
 
-    const responseMessage = (response) => {
-        console.log(response);
-    };
-    const errorMessage = (error) => {
-        console.log(error);
+    const login = useGoogleLogin({
+        onSuccess: (codeResponse) => setUser(codeResponse),
+        onError: (error) => console.log('Login Failed:', error)
+    });
+
+    useEffect(() => {
+        if (user) {
+            fetch(`https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`, {
+                headers: {
+                    Authorization: `Bearer ${user.access_token}`,
+                    Accept: 'application/json'
+                }
+            })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setProfile(data);
+            })
+            .catch((err) => console.log(err));
+        }
+    }, [user]);
+
+    // Log out function to log the user out of Google and set the profile to null
+    const logOut = () => {
+        googleLogout();
+        setProfile(null);
     };
 
     return (
         <PageContainer>
             <PageHeader title="Login" />
-            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
-
-            
+            {profile ? (
+                <div>
+                    <img src={profile.picture} alt="user" />
+                    <h3>User Logged in</h3>
+                    <p>Name: {profile.name}</p>
+                    <p>Email Address: {profile.email}</p>
+                    <br />
+                    <br />
+                    <button onClick={logOut}>Log out</button>
+                </div>
+            ) : (
+                <button onClick={() => login()}>Sign in with Google 🚀</button>
+            )}
         </PageContainer>
-    )
-}
+    );
+};
 
-export default Login 
+export default Login;
