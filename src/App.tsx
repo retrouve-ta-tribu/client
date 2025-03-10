f4ad6109e0c6ce137693563c41c7b900ef2b1b37
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-10 14:29:26 +0100

    feat: add a login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import GroupDetails from './pages/GroupDetails.jsx'
+import Login from './pages/Login.jsx'
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/group/:id" element={<GroupDetails />} />
       </Routes>
     </Router>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import { FC } from 'react'
+import PageContainer from '../components/layout/PageContainer'
+import PageHeader from '../components/layout/PageHeader'
+const Login: FC = () => {
+
+    return (
+        <PageContainer>
+            <PageHeader title="Login" />
+            
+        </PageContainer>
+    )
+}
+
+export default Login 
