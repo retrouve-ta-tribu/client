ab3e3a3a2283b3defe3a8bf5a723032f9a134431
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-10 14:48:42 +0100

    feat: add base login action

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <GoogleOAuthProvider clientId="299352643706-dfdb3ocngstgcaljg13pf05siun873mg.apps.googleusercontent.com" >
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </GoogleOAuthProvider>,
 )
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,22 @@
 import { FC } from 'react'
 import PageContainer from '../components/layout/PageContainer'
 import PageHeader from '../components/layout/PageHeader'
+import { GoogleLogin } from '@react-oauth/google';
+
 const Login: FC = () => {
+
+    const responseMessage = (response) => {
+        console.log(response);
+    };
+    const errorMessage = (error) => {
+        console.log(error);
+    };
 
     return (
         <PageContainer>
             <PageHeader title="Login" />
+            <GoogleLogin onSuccess={responseMessage} onError={errorMessage} />
+
             
         </PageContainer>
     )
