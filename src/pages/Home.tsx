58e18c838b9cd708ab71474058b79a8344305ff7
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-13 19:27:19 +0100

    feat: add the overflow behaviour in the group list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,24 +10,30 @@ const Home: FC = () => {
 
     return (
         <PageContainer>
-            <PageHeader title="Mes groupes" />
-            
-            <div className="p-4 flex justify-end">
-                <Link 
-                    to="/friends"
-                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
-                >
-                    Mes amis
-                </Link>
-            </div>
-            
-            <div className="divide-y divide-gray-100">
-                {groups.map((group) => (
-                    <GroupCard 
-                        key={group._id.$oid} 
-                        group={group} 
-                    />
-                ))}
+            <div className="flex flex-col h-full max-h-screen">
+                <div className="flex-shrink-0">
+                    <PageHeader title="Mes groupes" />
+                </div>
+                
+                <div className="p-4 flex justify-end flex-shrink-0">
+                    <Link 
+                        to="/friends"
+                        className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2"
+                    >
+                        Mes amis
+                    </Link>
+                </div>
+                
+                <div className="flex-grow overflow-y-auto">
+                    <div className="divide-y divide-gray-100">
+                        {groups.map((group, index) => (
+                            <GroupCard 
+                                key={`${group._id.$oid}-${index}`} 
+                                group={group} 
+                            />
+                        ))}
+                    </div>
+                </div>
             </div>
         </PageContainer>
     )
