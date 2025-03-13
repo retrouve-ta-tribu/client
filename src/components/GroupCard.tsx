1adb5c61d9c0676cbba8a9b4021e67167f5e727e
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-13 22:33:58 +0100

    refactor: replace the icon to leave the group in order to avoir confusion with 'delete'

--- src/components/GroupCard.tsx
+++ src/components/GroupCard.tsx
@@ -2,7 +2,7 @@ import { FC, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { Group } from '../services/groupService'
 import authService from '../services/authService'
-import Button from './ui/Button'
+import ExitIcon from './icons/ExitIcon'
 
 interface GroupCardProps {
     group: Group;
@@ -79,9 +79,7 @@ const GroupCard: FC<GroupCardProps> = ({ group, onLeave }) => {
                     }`}
                     title="Quitter le groupe"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                        <path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" />
-                    </svg>
+                    <ExitIcon className="h-5 w-5" />
                 </button>
             )}
         </div>
