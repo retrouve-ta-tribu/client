3770ed658a2a5714237efe5c9d87ad679d920ef3
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-14 23:50:39 +0100

    refactor: translate remainings label to english

--- src/components/groups/LocationUnavailable.tsx
+++ src/components/groups/LocationUnavailable.tsx
@@ -22,7 +22,7 @@ const LocationUnavailable: React.FC<LocationUnavailableProps> = ({
             clipRule="evenodd" 
           />
         </svg>
-        Location not available
+        Localisation non disponible
       </span>
     </div>
   );
--- src/components/groups/OnlineStatus.tsx
+++ src/components/groups/OnlineStatus.tsx
@@ -8,7 +8,7 @@ const OnlineStatus: React.FC<OnlineStatusProps> = ({ isOnline }) => {
   
   return (
     <span className={`ml-2 inline-flex items-center px-2 py-0.5 rounded text-xs font-medium ${isOnline ? 'bg-green-200 text-green-800' : 'bg-gray-200 text-gray-800'}`}>
-      {isOnline ? 'Online' : 'Offline'}
+      {isOnline ? 'En ligne' : 'Hors ligne'}
     </span>
   );
 };
