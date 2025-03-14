ece633037b38193dbd6795cd42cde1f88c56a959
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-14 22:09:01 +0100

    feat: add messages behaviours

--- src/components/Messages/Conversation.tsx
+++ src/components/Messages/Conversation.tsx
@@ -1,9 +1,22 @@
-import { FC, useState } from 'react';
+import { FC, useState, useEffect } from 'react';
 import Message from './Message';
+import SendIcon from '../icons/SendIcon';
 
 const mockedMessages = [
     {sender: "John Doe", content: "Hello"},
     {sender: "Will", content: "Hello my friend"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
+    {sender: "Patrick", content: "I can't see you!"},
     {sender: "Patrick", content: "I can't see you!"},
     {sender: "Le laitier", content: "Did you install spunk ?"},
     {sender: "Will", content: "OF COURSE !!!!!"},
@@ -14,6 +27,27 @@ interface ConversationProps {
 }
 
 const Conversation: FC<ConversationProps> = () => {
+    const [newMessage, setNewMessage] = useState("");
+
+    useEffect(() => {
+        const textarea = document.querySelector('textarea');
+        if (textarea) {
+            textarea.addEventListener('input', () => {
+                textarea.style.height = `${textarea.scrollHeight}px`;
+            });
+        }
+    }, [newMessage]);
+    
+
+    const handleWriteMessage = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setNewMessage(e.target.value);
+    }
+
+    const handleSendMessage = () => {
+        console.log(newMessage);
+        setNewMessage("");
+    }
+
 
     return (
         <>
@@ -27,14 +61,22 @@ const Conversation: FC<ConversationProps> = () => {
                     ))}
                 </div>
             </div>
-            <textarea 
+            
+            <div className="flex bg-white w-full flex-row absolute bottom-0 pb-8 px-4 gap-4 items-end">
+                <textarea 
                 placeholder="Message" 
-                className="w-full p-2 bg-white border border-gray-300 rounded-md absolute bottom-8 resize-none overflow-auto max-h-32 h-10 no-scrollbar"
-                onInput={(e) => {
-                    const target = e.target as HTMLTextAreaElement;
-                    target.style.height = `${target.scrollHeight}px`;
-                }}
-            />
+                className="w-full p-2 bg-white border border-gray-300 rounded-md resize-none overflow-auto max-h-32 h-10 no-scrollbar"
+                value={newMessage}
+                onChange={(e) => handleWriteMessage(e)}
+                />
+
+                <button 
+                    className="bg-indigo-500 text-white p-2 w-10 h-10 rounded-md hover:bg-indigo-600 cursor-pointer"
+                    onClick={() => handleSendMessage()}
+                >
+                    <SendIcon />
+                </button>
+            </div>
         </>
     );
 };
