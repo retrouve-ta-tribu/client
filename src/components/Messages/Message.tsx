321f3fe783f623f48a185c710cd9d68b3e5b3485
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-14 19:46:30 +0100

    feat: add message style depending on the sender

--- src/components/Messages/Message.tsx
+++ src/components/Messages/Message.tsx
@@ -4,12 +4,12 @@ interface MessageProps {
     children: React.ReactNode;
 }
 
-const Message: FC<MessageProps> = ({message}) => {
+const Message: FC<MessageProps> = ({message, isSent = false}) => {
 
     return (
-        <div className="flex w-full">
-            <div className="max-w-4/5 bg-gray-200 p-2 rounded-lg text-wrap break-words flex flex-col">
-                <span className="font-bold text-sm text-gray-500">{message.sender}</span>
+        <div className={`flex ${isSent? 'justify-end': 'justify-start'} w-full`}>
+            <div className={`${isSent? 'bg-indigo-200': 'bg-gray-200 '} max-w-4/5  p-2 rounded-lg text-wrap break-words flex flex-col`}>
+                <span className="font-bold text-sm text-gray-600">{message.sender}</span>
                 <span>{message.content}</span>
             </div>
         </div>
