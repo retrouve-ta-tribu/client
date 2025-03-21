4fd04f04058bd21ca98f24e2652bfd6adbe11cc7
eliott <eliott.jaquier@gmail.com>
2025-03-22 00:37:10 +0100

    feat: increase maximum width of PageContainer for improved layout flexibility

--- src/components/layout/PageContainer.tsx
+++ src/components/layout/PageContainer.tsx
@@ -6,7 +6,7 @@ interface PageContainerProps {
 
 const PageContainer: FC<PageContainerProps> = ({ children }) => {
   return (
-    <div className="max-w-3xl mx-auto bg-white shadow-md h-screen flex flex-col overflow-hidden">
+    <div className="max-w-6xl mx-auto bg-white shadow-md h-screen flex flex-col overflow-hidden">
       {children}
     </div>
   );
