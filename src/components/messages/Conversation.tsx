08ba06b6173d5d3461cb11460c1d55ef3dce27d9
Cyprien Jaquier <61788240+Cyprien-png@users.noreply.github.com>
2025-03-24 19:27:15 +0100

    fix: repair the chat scroll

--- src/components/messages/Conversation.tsx
+++ src/components/messages/Conversation.tsx
@@ -94,7 +94,7 @@ const Conversation: FC<ConversationProps> = ({ group, setHasUnreadMessage }) => 
     return (
         <>
             <div className="flex flex-col h-full">
-                <div className="flex-1 flex overflow-auto no-scrollbar justify-end items-end">
+                <div className="block h-full overflow-auto no-scrollbar justify-end items-end">
                     <div className="flex px-4 flex-col gap-4 w-full">
                         {messages.map((message, index) => (
                             <Message 
